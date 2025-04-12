fbd9ae345d9cd492417650459d976b3b286a70b1
Nitish <nitishappireddypally@gmail.com>
2025-04-12 14:42:07 +0530

    Frontend authentication

--- client/src/components/Auth/Signup.jsx
+++ client/src/components/Auth/Signup.jsx
@@ -1,9 +1,37 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Signup = () => {
   const containerRef = useRef(null);
   const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail]= useState("");
+  const [password, setPassword] = useState("");
+  const [showPass, setShowPass]=useState(false);
+  const [role, setRole]= useState("");
+
+  const handleSignup = async (e)=>{
+    e.preventDefault();
+    try{
+      const response = await axios.post(
+        "http://localhost:5050/api/auth/register",
+        {
+          name,
+          email,
+          password,
+          role,
+        }
+      );
+      const {token, user} = response.data;
+      navigate("/dashboard");
+    }
+    catch(error){
+      console.error("Signup Failed");
+      alert("Signup failed : "+(error.response?.data?.message || "Server error"))
+    }
+  }
+
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -22,13 +50,15 @@ const Signup = () => {
     <div className="min-h-screen flex items-center justify-center bg-[#F9FAFB] px-4">
       <div ref={containerRef} className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
         <h2 className="text-2xl font-bold text-[#1F2937] mb-6 text-center">Create an Account</h2>
-        <form>
+        <form onSubmit={handleSignup}>
           <div className="mb-4">
             <label className="block text-[#1F2937] mb-1">Name</label>
             <input
               type="text"
               className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#FBBF24]"
               placeholder="John Doe"
+              value={name}
+              onChange={(e)=> setName(e.target.value)}
             />
           </div>
           <div className="mb-4">
@@ -37,14 +67,34 @@ const Signup = () => {
               type="email"
               className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#FBBF24]"
               placeholder="you@example.com"
+              value={email}
+              onChange={(e)=> setEmail(e.target.value)}
             />
           </div>
-          <div className="mb-6">
+          <div className="mb-6 relative">
             <label className="block text-[#1F2937] mb-1">Password</label>
             <input
-              type="password"
+              type={showPass? "text":"password"}
               className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#FBBF24]"
               placeholder="••••••••"
+              value={password}
+              onChange={(e)=> setPassword(e.target.value)}
+            />
+            <span
+            onClick={()=> setShowPass(!showPass)}
+            className='absolute right-3 top-10 cursor-pointer text-sm text-gray-400'
+            >
+              {showPass? "Hide": "Show"}
+            </span>
+          </div>
+          <div className="mb-4">
+            <label className="block text-[#1F2937] mb-1">Role</label>
+            <input
+              type="text"
+              className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#FBBF24]"
+              placeholder="John Doe"
+              value={role}
+              onChange={(e)=>setRole(e.target.value)}
             />
           </div>
           <button
