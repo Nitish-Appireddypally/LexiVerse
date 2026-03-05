e2be635a1aea2561f92fc16d5dbac89661de4d09
Nitish <nitishappireddypally@gmail.com>
2026-03-06 01:30:46 +0530

    db.js changed to neon

--- server/config/db.js
+++ server/config/db.js
@@ -3,15 +3,19 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-pool.connect()
-  .then(() => console.log("✅ Connected to PostgreSQL"))
-  .catch(err => console.error("❌ Database connection error:", err.message));
+pool
+  .connect()
+  .then(() => {
+    console.log("✅ Connected to PostgreSQL (Neon)");
+  })
+  .catch((err) => {
+    console.error("❌ Database connection error:", err.message);
+  });
 
 module.exports = pool;
